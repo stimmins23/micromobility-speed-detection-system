3242b44380513d04df1904056fbae0ccb1afb3c2
Samantha Timmins <stimmins234@gmail.com>
2026-04-14 09:16:21 -0400

    - updated .gitignore

--- src/models/speed_event_model.py
+++ src/models/speed_event_model.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional
-<<<<<<< HEAD
-
 
 @dataclass
 class SpeedEvent:
@@ -12,15 +10,3 @@ class SpeedEvent:
     threshold_value: float
     image_path: Optional[str]
     location: str
-=======
-
-@dataclass
-class SpeedEvent:
-    id: int 
-    timestamp: datetime 
-    speed_mph: float 
-    threshold_value: float 
-    image_path: Optional[str]
-    location: str
-
->>>>>>> 75f5325e05d17dc3b872a470d28561c670910056
